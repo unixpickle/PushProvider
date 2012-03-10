7d6812e699573401685a4b7e15b8d724133fdfdc
Alex Nichol <alexnichol@comcast.net>
2012-03-10 16:05:46 -0500

    Make resources dir if it doesn't exist

--- main.rb
+++ main.rb
@@ -4,6 +4,9 @@ require 'ReqPath'
 require 'Listener'
 
 $APN_RESOURCES = './resources'
+if !File.directory?($APN_RESOURCES)
+	Dir.mkdir($APN_RESOURCES)
+end
 
 provider = APN::Provider.new()
 listener = ClientListener.new(provider, 1337)
